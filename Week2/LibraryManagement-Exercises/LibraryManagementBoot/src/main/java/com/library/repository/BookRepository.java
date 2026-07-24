c984967210e9f03a02d8d12985448d67e562b8e4
Harsh Kumar Rai <150253628+Harsh-60@users.noreply.github.com>
2026-07-24 14:39:52 +0530

    Delete Week2/LibraryManagement-Exercises directory

--- Week2/LibraryManagement-Exercises/LibraryManagementBoot/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-import com.library.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// JpaRepository already gives us save/findAll/findById/delete etc, nothing else to add here
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
